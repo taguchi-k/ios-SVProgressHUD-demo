5dde18a58bbde791e43fabbcf07ec7a87cfacff6
taguchi-k <k.taguchi@st-ventures.jp>
2017-03-09 23:54:45 +0900

    Initial Commit

---
+++ ios-SVProgressHUD-demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-SVProgressHUD-demo
+//
+//  Created by Kentaro on 2017/03/09.
+//  Copyright © 2017年 Kentao Taguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
